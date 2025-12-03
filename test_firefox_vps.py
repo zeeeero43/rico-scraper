a6588ab2d3b407b30abcfb68819256892d03118b
zerocrafter1 <zerocrafter1@hotmail.com>
2025-12-03 15:46:23 +0100

    Add Firefox test script for VPS

---
+++ test_firefox_vps.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.binary_location = '/snap/bin/firefox'
+
+print('Starting Firefox...')
+driver = webdriver.Firefox(options=options)
+print('Firefox started!')
+driver.get('https://google.com')
+print('Title:', driver.title)
+driver.quit()
+print('Success!')
